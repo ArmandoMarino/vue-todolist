75d2b0fc7aa91ff04f8ab17320d0ddf00032600e
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-01-11 16:26:02 +0100

    101 html css

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log('JS OK');
+console.log(Vue + "Vue OK");
+
+const app = Vue.createApp({
+    data() {
+        return{
+            
+        }
+    },
+  });
+  
+  
+  app.mount('#root');
