747e8b46ecaf1223e7fb3d3dccb148731ea83128
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-01-11 17:13:59 +0100

    addtask , delete and clearimput

--- script.js
+++ script.js
@@ -8,9 +8,23 @@ const app = Vue.createApp({
                 'Fare i compiti',
                 'Fare la spesa',
                 'Fare il bucato'
-            ]
+            ],
+            newTask : "",
+            done : true,
         }
     },
+    methods:{
+        addTask(){
+           this.tasks.push(this.newTask);
+           this.clearInput(); 
+        },
+        deleteTask(index){
+            this.tasks.splice(index, 1);
+        },
+        clearInput(){
+            this.newTask = "";
+        }
+    }
   });
   
   
